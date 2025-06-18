983fe09721128907340decb6c3256a9c53db8e68
John Galt <johngaltfirstrun@gmail.com>
2025-06-18 10:10:20 -0400

    waffle: vibrator: effect: Implement primitive effects
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: I2a3936035e3deba66d25c443977ce3b0b85aa300

--- device.mk
+++ device.mk
@@ -79,8 +79,10 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
-$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oneplus_waffle-richtap)
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set_bool,qti_vibrator,use_effect_stream_strength,true)
+$(call soong_config_set_bool,qti_vibrator,use_primitive_effect_stream,true)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oneplus_waffle-richtap)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
