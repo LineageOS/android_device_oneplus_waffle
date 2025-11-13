f357640f7d644c938b6a8171250ebce08f39febf
chandu078 <chandudyavanapelli03@gmail.com>
2025-11-13 18:44:27 +0000

    waffle: Update from OOS 11.F.33
    Change-Id: I24b18ec6796ae071499b248ef382d93942053015

--- lineage_waffle.mk
+++ lineage_waffle.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2581
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 15 AP3A.240617.008 1759673810118 release-keys" \
-    BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:15/AP3A.240617.008/U.R4T3.cfc6a2_1337040_1337041:user/release-keys \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1761898973141 release-keys" \
+    BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:16/BP2A.250605.015/U.R4T3.39575dc-1907a57-1939ec6:user/release-keys \
     DeviceName=OP595DL1 \
     DeviceProduct=CPH2581 \
     SystemDevice=OP595DL1 \
